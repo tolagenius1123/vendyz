fbd7027ca881ccb603cdbb3c4496f9bc7869299f
Omotola Jinadu <91120863+tolagenius1123@users.noreply.github.com>
2024-11-21 11:06:29 +0100

    resolved get request error

--- src/config/actions/post-actions.ts
+++ src/config/actions/post-actions.ts
@@ -3,7 +3,10 @@ import { AppConfig } from "..";
 
 export const getPosts = async () => {
 	try {
-		const response = await axios.get(`${AppConfig.POST_URL}`);
+		// const response = await axios.get(`${AppConfig.POST_URL}`);
+		const response = await axios.get(
+			`https://jsonplaceholder.typicode.com/posts`
+		);
 		return response.data;
 	} catch (error) {
 		throw error;
